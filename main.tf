9a917ad9665022bbeccb920b6e47a3486df85e10
venkatpodaralla <84852520+venkatpodaralla@users.noreply.github.com>
2022-12-21 02:15:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,5 @@
 
-data "aws_vpc" "selected" {
-  id = var.vpc_id
-}
-
 resource "aws_vpc_ipv4_cidr_block_association" "secondary_cidr" {
-  vpc_id     = var.vpc_id
+  vpc_id     = var.aws_vpc
   cidr_block = "10.1.0.0/26"
 }
