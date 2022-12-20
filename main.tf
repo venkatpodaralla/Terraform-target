3fcc973ab18263e553b02f9e0b353c138dc131e6
venkatpodaralla <84852520+venkatpodaralla@users.noreply.github.com>
2022-12-21 03:02:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,9 +6,17 @@ resource "aws_vpc_ipv4_cidr_block_association" "secondary_cidr" {
 resource "aws_subnet" "MY-subnet-1" {
   vpc_id     = aws_vpc_ipv4_cidr_block_association.secondary_cidr.vpc_id
   cidr_block = var.subnet-1a
+
+  tags = {
+    Name = "My-subnet-1"
+  }
 }
 
 resource "aws_subnet" "MY-subnet-2" {
   vpc_id     = aws_vpc_ipv4_cidr_block_association.secondary_cidr.vpc_id
   cidr_block = var.subnet-1b
+  
+  tags = {
+    Name = "My-subnet-2"
+  }
 }
