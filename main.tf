dd8260fe3d74aad290bba137f17947b3cf4abe80
venkatpodaralla <84852520+venkatpodaralla@users.noreply.github.com>
2022-12-24 23:03:25 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,29 +1,26 @@
 resource "aws_vpc_ipv4_cidr_block_association" "secondary_cidr" {
   vpc_id     = var.aws_vpc
-  cidr_block = "10.1.0.0/26"
+  cidr_block = var.secondary_cidr
+
+   tags = {
+    Name = join("-", [var.project_name, "vpc"])
+  }
+
 }
 
-resource "aws_subnet" "MY-subnet-1" {
-  vpc_id     = aws_vpc_ipv4_cidr_block_association.secondary_cidr.vpc_id
-  cidr_block = var.subnet-1a
+resource "aws_subnet" "private" {
+  count = length(var.private_subnets) > 0 ? length(var.private_subnets) : 0
+
+  vpc_id                          = aws_vpc.main.id
+  cidr_block                      = var.private_subnets[count.index]
+  availability_zone               = length(regexall("^[a-z]{2}-", element(var.availability_zones, count.index))) > 0 ? element(var.availability_zones, count.index) : null
+  availability_zone_id            = length(regexall("^[a-z]{2}-", element(var.availability_zones, count.index))) == 0 ? element(var.availability_zones, count.index) : null
 
   tags = {
-    Name = "My-subnet-1"
-  }
+      "Name" = format(
+        "${var.project_name}-private-sn-%s",
+        element(var.availability_zones, count.index),
+      )
+    }
 }
 
-resource "aws_subnet" "MY-subnet-2" {
-  vpc_id     = aws_vpc_ipv4_cidr_block_association.secondary_cidr.vpc_id
-  cidr_block = var.subnet-1b
-  
-  tags = {
-    Name = "My-subnet-2"
-  }
-}
-resource "aws_route_table" "private" {
-  vpc_id = var.aws_vpc
-
-  tags = {
-    Name = "My_route_table"
-  }
-}
