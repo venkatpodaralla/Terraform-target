e2246b8b2e80d5548e9fc9a0dff6d373b9f6767c
venkatpodaralla <84852520+venkatpodaralla@users.noreply.github.com>
2022-12-24 23:04:26 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,10 +2,12 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
+      version = "~> 3.0"
     }
   }
 }
-
 provider "aws" {
-  region  = "us-east-1"
+  region = var.region
 }
+
+
