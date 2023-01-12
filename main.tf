1f2f21d9ea62e2d9b2971051dc5d4e3b05b4a537
venkatpodaralla <84852520+venkatpodaralla@users.noreply.github.com>
2023-01-12 12:56:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_vpc_ipv4_cidr_block_association" "secondary_cidr" {
 resource "aws_subnet" "private" {
   count = length(var.private_subnets) > 0 ? length(var.private_subnets) : 0
 
-  vpc_id               = var.aws_vpc
+  vpc_id               = aws_vpc_ipv4_cidr_block_association.secondary_cidr.vpc_id
   cidr_block           = var.private_subnets[count.index]
   availability_zone    = length(regexall("^[a-z]{2}-", element(var.availability_zones, count.index))) > 0 ? element(var.availability_zones, count.index) : null
   availability_zone_id = length(regexall("^[a-z]{2}-", element(var.availability_zones, count.index))) == 0 ? element(var.availability_zones, count.index) : null
